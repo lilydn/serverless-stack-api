dda4f6706d1f92f2a52a9f3da381f3293ab791e4
lilydn <kaganli404@gmail.com>
2020-11-16 16:57:00 +0200

    Adding CORS to our Serverless API

---
+++ libs/billing-lib.js
@@ -0,0 +1,10 @@
+// our business logic
+export function calculateCost(storage) {
+  const rate = storage <= 10
+    ? 4
+    : storage <= 100
+      ? 2
+      : 1;
+
+  return rate * storage * 100;
+}
